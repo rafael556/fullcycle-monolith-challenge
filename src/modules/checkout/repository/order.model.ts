3245faa9de62d680aaf6a54fea0f375363854243
rafael556 <rafael7jf@hotmail.com>
2024-10-30 14:53:09 -0300

    feat: create checkout sequelize models

---
+++ src/modules/checkout/repository/order.model.ts
@@ -0,0 +1,30 @@
+import { Column, ForeignKey, HasMany, Model, PrimaryKey, Table } from "sequelize-typescript";
+import Product from "../domain/product.entity";
+import { ClientModel } from "./client.model";
+import ProductModel from "./product.model";
+
+@Table({
+    tableName: 'orders',
+    timestamps: false
+})
+export default class OrderModel extends Model {
+    @PrimaryKey
+    @Column({allowNull: false})
+    id: string;
+
+    @ForeignKey(() => ClientModel)
+    @Column({allowNull: false})
+    clientId: string;
+
+    @HasMany(() => ProductModel)
+    products: Product[];
+
+    @Column({allowNull: false})
+    status: string;
+
+    @Column({allowNull: false})
+    createdAt: Date;
+
+    @Column({allowNull: false})
+    updatedAt: Date;
+}
